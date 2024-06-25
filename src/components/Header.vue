23b66a5bd64dfbbb3aa59a3b329e10a486261985
Guilherme Tinen <gfmi0911@hotmail.com>
2024-06-25 12:32:12 -0300

    header

---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex w-full justify-end absolute top-0 z-10 px-[100px] h-[75px]">
+    <div class="flex items-center gap-[72px] text-white">
+      <a class="font-medium font-inter uppercase" href="">Why Us</a>
+      <a class="font-medium font-inter uppercase" href="">Travel Blog</a>
+      <div class="flex items-center gap-3">
+        <div class="w-[27px] h-4 rounded-sm bg-white"></div>
+        <span class="uppercase text-xs font-inter font-medium">English</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
