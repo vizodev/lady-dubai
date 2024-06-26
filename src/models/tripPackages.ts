eb699f007e600fc69caaede1db5671a8109dee95
Guilherme Tinen <gfmi0911@hotmail.com>
2024-06-25 22:39:05 -0300

    trip package model fix

--- src/models/tripPackages.ts
+++ src/models/tripPackages.ts
@@ -1,10 +1,10 @@
 export type TripPackage = {
-  banners: string[];
+  gallery: string[];
+  tag: string;
   title: string;
   subtitle: string;
-  headline: string;
-  guideLanguages: string[];
   services: PackagesServices[];
+  guideLanguages: string[];
   price: {
     usd: number;
     ils: number;
@@ -15,6 +15,14 @@ export type TripPackage = {
     ils: number;
     aed: number;
   };
+  nextAvailableDates: {
+    from: Date;
+    to: Date;
+  }[];
+  cancelationPolicy: string[];
+  disclaimer: string[];
+  // Specific to the package card
+  headline: string;
 };
 
 export enum PackagesServices {
@@ -44,7 +52,11 @@ export enum GuideLanguages {
 }
 
 export const defaultTripPackage: TripPackage = {
-  banners: ["https://picsum.photos/1920/1080"],
+  gallery: [
+    "https://picsum.photos/1920/1080",
+    "https://picsum.photos/768/640",
+    "https://picsum.photos/540/320",
+  ],
   title: "The Dubai Experience",
   subtitle: "5 Day All Inclusive Tour",
   headline: "Explore the main turistic attractions of the city.",
@@ -70,4 +82,42 @@ export const defaultTripPackage: TripPackage = {
     ils: 10399,
     aed: 10399,
   },
+  tag: "Open Gallery",
+  nextAvailableDates: [
+    // 18 jan 2025 to 24 jan 2025
+    {
+      from: new Date(2025, 0, 18),
+      to: new Date(2025, 0, 24),
+    },
+    // 19 feb 2025 to 24 feb 2025
+    {
+      from: new Date(2025, 1, 19),
+      to: new Date(2025, 1, 24),
+    },
+    // 29 may 2025 to 03 jun 2025
+    {
+      from: new Date(2025, 4, 29),
+      to: new Date(2025, 5, 3),
+    },
+    // 29 dec 2025 to 03 jan 2026
+    {
+      from: new Date(2025, 11, 29),
+      to: new Date(2026, 0, 3),
+    },
+    // 19 feb 2026 to 24 feb 2026
+    {
+      from: new Date(2026, 1, 19),
+      to: new Date(2026, 1, 24),
+    },
+  ],
+  cancelationPolicy: [
+    "Up to 72 hours after booking - free of charge.",
+    "Any cancelation request made 72 hours after booking carries a USD 500 fee",
+  ],
+  disclaimer: [
+    "Travelers' insurance and health insurance are not included.",
+    "Airline allows 1 carry-on bag up to 8 kg and one luggage up to 20 kg.",
+    "Waiver's must be signed when booking this tour.",
+    "Extreme Sports insurance is mandatory.",
+  ],
 };
