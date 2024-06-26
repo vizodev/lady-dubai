c4ce9ba620a97f4308cba9d74c452480c6aeaa89
Guilherme Tinen <gfmi0911@hotmail.com>
2024-06-26 12:00:50 -0300

    footer responsive

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,12 @@
 <template>
-  <footer class="flex flex-col px-[72px] gap-[52px] py-[65px]">
-    <div class="flex items-baseline justify-between px-[9%]">
-      <div class="flex flex-col">
-        <div class="flex flex-col gap-1.5 text-brown-700">
+  <footer class="flex flex-col px-6 lg:px-[72px] gap-8 lg:gap-[52px] py-[65px]">
+    <div
+      class="flex items-center justify-between px-6 xl:px-[9%] flex-col-reverse md:flex-row gap-8 md:gap-0"
+    >
+      <div class="flex flex-col gap-2 items-center md:items-start">
+        <div
+          class="flex flex-col gap-1.5 text-brown-700 items-center md:items-start"
+        >
           <span class="font-inter font-bold uppercase">Customer service</span>
           <span class="font-inter font-light"
             >Available sunday to friday, 10am CT to 16pm CT</span
@@ -10,12 +14,14 @@
         </div>
         <div class="flex items-center gap-3">
           <img src="~/assets/imgs/whatsapp-icon.svg" alt="" class="w-6" />
-          <span class="font-inter font-medium text-[24px]"
-            >+55 813-369-5388</span
-          >
+          <span class="font-inter font-medium text-[18px] xl:text-[24px]">
+            +55 813-369-5388
+          </span>
         </div>
       </div>
-      <div class="flex items-center gap-10">
+      <div
+        class="flex items-center gap-6 lg:gap-10 flex-col-reverse lg:flex-row"
+      >
         <div class="flex items-center gap-[28px]">
           <button class="socialShadow">
             <img src="~/assets/imgs/facebook-icon.svg" alt="" />
@@ -33,17 +39,31 @@
       </div>
     </div>
     <div class="w-full h-[1px] bg-brown-700"></div>
-    <div class="flex items-center justify-between px-[9%]">
-      <span class="font-inter font-light"
-        >LADYDUBAI Copyright © 2024 Lady Dubai. All rights reserved</span
-      >
-      <span class="font-inter font-light">Privacy Policy</span>
-      <span class="font-inter font-light">Terms of Use</span>
-      <div class="flex items-center gap-6">
-        <LanguageFlag :language="'English'" />
-        <LanguageFlag :language="'Hebraic'" />
-        <LanguageFlag :language="'Arabic'" />
-        <LanguageFlag :language="'Russian'" />
+    <div
+      class="flex items-center justify-between px-6 xl:px-[9%] flex-col lg:flex-row gap-4 lg:gap-0"
+    >
+      <span class="font-inter font-light text-center sm:text-start">
+        LADYDUBAI Copyright © 2024 Lady Dubai. All rights reserved
+      </span>
+      <div class="flex flex-col gap-6 items-center lg:items-end">
+        <div class="flex items-center gap-6">
+          <a
+            href="*"
+            class="font-inter font-light hover:text-blue-500 hover:underline transition-all duration-[.2s]"
+            >Privacy Policy</a
+          >
+          <a
+            href="*"
+            class="font-inter font-light hover:text-blue-500 hover:underline transition-all duration-[.2s]"
+            >Terms of Use</a
+          >
+        </div>
+        <div class="flex items-center gap-6 flex-wrap">
+          <LanguageFlag :language="'English'" />
+          <LanguageFlag :language="'Hebraic'" />
+          <LanguageFlag :language="'Arabic'" />
+          <LanguageFlag :language="'Russian'" />
+        </div>
       </div>
     </div>
   </footer>
--- src/components/LanguageFlag.vue
+++ src/components/LanguageFlag.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="flex items-center gap-3">
+  <button
+    class="flex items-center gap-3 duration-[.2s] text-brown-700 hover:!text-blue-500"
+  >
     <div class="w-[27px] h-[16px] rounded-sm bg-brown-700"></div>
-    <span class="uppercase font-inter font-medium text-xs text-brown-700">{{
-      language
-    }}</span>
-  </div>
+    <span class="uppercase font-inter font-medium text-xs">{{ language }}</span>
+  </button>
 </template>
 
 <script lang="ts" setup>
