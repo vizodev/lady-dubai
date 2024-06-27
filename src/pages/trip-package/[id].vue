2b8c61daf525f840f36dc226364a7998ee7f72ef
Guilherme Tinen <gfmi0911@hotmail.com>
2024-06-27 18:24:35 -0300

    Trip page and edge card

--- src/pages/trip-package/[id].vue
+++ src/pages/trip-package/[id].vue
@@ -1,11 +1,35 @@
 <template>
-  <div class="flex flex-col">
+  <div class="flex flex-col overflow-y-auto w-full">
     <TripPackageGallery :gallery="currentTripPackage?.gallery ?? []" />
-    <div
-      class="flex items-stretch w-full justify-between px-[245px] pt-[47px] relative z-10"
+    <section
+      class="flex items-stretch w-full justify-between px-[245px] pt-[47px] relative z-10 pb-16"
     >
-      <div class="flex flex-col max-w-[600px]">
+      <div class="flex flex-col max-w-[600px] gap-[52px]">
         <div class="flex flex-col gap-8">
+          <div class="flex gap-3">
+            <button
+              class="flex gap-3 items-center"
+              v-for="(path, index) in relativePath"
+              :class="{
+                'text-pink-600': index < relativePath.length - 1,
+                'text-[#272223]': index === relativePath.length - 1,
+              }"
+              :disabled="index === relativePath.length - 1"
+              @click="path.path?.length ? navigateTo(path.path) : null"
+            >
+              <span
+                class="text-[12px] leading-tight font-medium font-inter uppercase hover:underline font-medium font-inter uppercase"
+              >
+                {{ path.label.en }}
+              </span>
+              <div
+                class="flex items-center justify-center h-full"
+                v-if="index < relativePath.length - 1"
+              >
+                <i class="fi fi-sr-angle-right text-[12px]"></i>
+              </div>
+            </button>
+          </div>
           <div class="flex flex-col gap-0.5">
             <span
               class="font-medium text-[52px] text-[#272223] leading-tight font-roboto-serif"
@@ -20,62 +44,151 @@
             {{ currentTripPackage?.description }}
           </span>
         </div>
-      </div>
-      <div
-        class="flex flex-col w-full max-w-[528px] bg-white shadow-md pt-8 pb-[52px] px-6 rounded-[16px] -mt-[calc(47px+35px)] items-center"
-      >
-        <div class="flex flex-col gap-6 items-center">
-          <div class="flex flex-col items-center gap-1">
-            <span
-              class="text-[#749BFF] font-bold text-[20px] leading-tight font-inter"
-            >
-              Limited Time Promotional Price
+        <div class="flex flex-col gap-12">
+          <div class="flex flex-col gap-3">
+            <span class="uppercase font-bold font-inter leading-tight">
+              What’s included
             </span>
-            <div class="relative w-fit">
-              <span
-                class="text-brown-700 font-semibold font-roboto-serif text-[24px]"
-              >
-                {{ `$ ${currentTripPackage?.downSalePrice?.usd}` }}
-              </span>
+            <div class="flex gap-6 flex-wrap">
               <div
-                class="w-full h-0.5 bg-blue-200 absolute rotate-[15deg] left-0 z-[1] top-[50%] translate-y-[-50%]"
-              ></div>
-            </div>
-            <div
-              class="flex items-center gap-1 bg-blue-200 text-white rounded-[10px] px-1.5 py-1"
-            >
-              <i class="fi fi-br-clock-three flex"></i>
-              <span class="text-sm font-bold font-inter">
-                7 Days, 23H, 13m
-              </span>
+                class="flex items-center gap-1.5"
+                v-for="serv in packagesServicesList"
+              >
+                <img :src="serv.icon" alt="" class="w-6" />
+                <span class="font-medium font-inter leading-tight">
+                  {{ serv.label.en }}
+                </span>
+              </div>
             </div>
           </div>
-          <div class="flex items-center gap-3">
-            <span
-              class="text-pink-500 font-bold font-roboto-serif text-[28px] sm:text-[32px] md:text-[36px] xl:text-[48px] whitespace-nowrap"
-            >
-              {{ `$ ${currentTripPackage?.price?.usd}` }}
+          <div class="flex flex-col gap-3">
+            <span class="uppercase font-bold font-inter leading-tight">
+              Available guide languages
             </span>
-            <div class="flex flex-col font-inter">
-              <span class="font-bold text-pink-600 leading-none"> DIRHAM </span>
-              <span class="text-[14px] font-medium text-pink-500">
-                per person
+            <div
+              class="flex flex-wrap gap-5 items-center w-full"
+              v-if="currentTripPackage"
+            >
+              <span
+                class="border border-[#272223] text-[#272223] font-medium font-inter px-1.5 py-1 rounded-md"
+                v-for="language in currentTripPackage.guideLanguages"
+              >
+                {{ language }}
               </span>
             </div>
           </div>
         </div>
       </div>
+      <TripPackagePageEdgeCard
+        v-if="currentTripPackage"
+        :trip-package="currentTripPackage"
+      />
+    </section>
+    <div class="flex flex-col w-full px-[245px] py-[52px] gap-8">
+      <span class="font-roboto-serif font-medium text-[36px] leading-tight">
+        Attractions
+      </span>
+      <Attractions />
+      <Accommodations />
     </div>
+    <div
+      class="flex flex-col gap-3 w-full bg-pink-100 items-center py-[26px] my-[52px]"
+    >
+      <span
+        class="text-[52px] font-roboto-serif font-medium leading-tight text-[#272223]"
+      >
+        Check Other Packages
+      </span>
+      <div class="flex items-center gap-[82px] text-pink-600">
+        <button
+          class="flex items-center gap-1.5"
+          v-if="previousPackage"
+          @click="navigateTo(`/trip-package/${previousPackage.id}`)"
+        >
+          <div class="flex items-center justify-center w-9 h-9">
+            <i class="fi fi-sr-angle-left text-[12px]"></i>
+          </div>
+          <span class="text-[24px] font-medium font-roboto-serif leading-tight">
+            {{ previousPackage.title }}
+          </span>
+        </button>
+        <button
+          class="flex items-center gap-1.5"
+          v-if="nextPackage"
+          @click="navigateTo(`/trip-package/${nextPackage.id}`)"
+        >
+          <span
+            class="text-[24px] font-medium font-roboto-serif leading-tight"
+            >{{ nextPackage.title }}</span
+          >
+          <div class="flex items-center justify-center w-9 h-9">
+            <i class="fi fi-sr-angle-right text-[12px]"></i>
+          </div>
+        </button>
+      </div>
+    </div>
+    <Contact />
+    <Footer />
   </div>
 </template>
 
 <script lang="ts" setup>
+import { packagesServicesList } from "~/models";
 const tripPackagesStore = useTripPackagesStore();
 const { loadingTripPackages, errorOnLoadTripPackages, tripPackages } =
   storeToRefs(tripPackagesStore);
 
+const previousPackage = computed(() => {
+  const index = tripPackages.value.findIndex(
+    (tripPackage) => tripPackage.id === packId.value
+  );
+  return tripPackages.value[index - 1];
+});
+
+const nextPackage = computed(() => {
+  const index = tripPackages.value.findIndex(
+    (tripPackage) => tripPackage.id === packId.value
+  );
+  return tripPackages.value[index + 1];
+});
+
 const packId = computed(() => {
   return useRoute().params.id;
+});
+
+const relativePath = ref<
+  {
+    label: {
+      en: string;
+      he: string;
+    };
+    path?: string;
+  }[]
+>([]);
+
+onMounted(() => {
+  relativePath.value = [
+    {
+      label: {
+        en: "Lady Dubai",
+        he: "ליידי דובאי",
+      },
+      path: "/",
+    },
+    {
+      label: {
+        en: "All Packages",
+        he: "כל החבילות",
+      },
+      path: "/",
+    },
+    {
+      label: {
+        en: currentTripPackage.value?.title ?? "",
+        he: currentTripPackage.value?.title ?? "",
+      },
+    },
+  ];
 });
 
 const currentTripPackage = computed(() => {
