7c824a130369889c6decf476d28347336d1d9955
Guilherme Ortolani <gfmi0911@hotmail.com>
2024-07-09 20:35:15 -0300

    contact btn

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -7,6 +7,16 @@
     <ContactV2 />
   </div>
   <Footer />
+  <div
+    v-if="showContact"
+    class="fixed bottom-[80px] right-20 animate-bounce hover:animate-none transition-all duration-[.2s]"
+  >
+    <button
+      class="flex items-center justify-center bg-[#21B65A] rounded-full w-[52px] h-[52px] text-white text-[24px] customShadow"
+    >
+      <i class="fi fi-brands-whatsapp"></i>
+    </button>
+  </div>
 </template>
 
 <script lang="ts" setup>
@@ -14,7 +24,24 @@ const tripPackagesStore = useTripPackagesStore();
 const { loadingTripPackages, errorOnLoadTripPackages } =
   storeToRefs(tripPackagesStore);
 
+const showContact = ref(false);
+
 onMounted(async () => {
   await tripPackagesStore.loadTripPackages();
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 150) {
+      showContact.value = true;
+    } else {
+      showContact.value = false;
+    }
+  });
 });
 </script>
+
+<style scoped>
+.customShadow {
+  box-shadow: 0px 0px 2px rgba(118, 32, 78, 0.15),
+    0px 4px 32px rgba(119, 51, 67, 0.05);
+}
+</style>
