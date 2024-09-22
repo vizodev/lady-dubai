0bd039f6cf8016ee60941096a57beabfebfba4ce
fabiogabrielcb <pentestfinal2006@gmail.com>
2024-09-22 15:02:44 +0300

    fix: yt loop

--- src/components/VideoRender.vue
+++ src/components/VideoRender.vue
@@ -1,14 +1,44 @@
 <template>
-	<iframe v-if="isYoutubeVideoUrl" :src="src" frameborder="0"></iframe>
+	<!-- <iframe v-if="isYoutubeVideoUrl" :src="src" frameborder="0"></iframe> -->
+	<ScriptYouTubePlayer
+		v-if="isYoutubeVideoUrl && youtubeVideoData"
+		trigger="immediate"
+		:video-id="youtubeVideoData.id"
+		:player-vars="youtubeVideoData.vars"
+		@ready="onReady"
+		@state-change="onStateChange"
+	/>
 	<video v-else controls autoplay class="bg-off-black">
 		<source :src="src" />
 	</video>
 </template>
 
 <script setup lang="ts">
+import { getYoutubeVideoUrlData } from "~/utils/getYoutubeVideoUrlData"
+
 const { src } = defineProps<{
 	src?: string
 }>()
 
+// General
 const isYoutubeVideoUrl = isYoutubeUrl(src)
+
+// Youtube Video
+const youtubeVideoData =
+	isYoutubeVideoUrl && src ? getYoutubeVideoUrlData(src) : null
+
+const onReady = (e: YT.PlayerEvent) => {
+	e.target.playVideo()
+}
+const onStateChange = (e: YT.OnStateChangeEvent) => {
+	if (!youtubeVideoData) return
+
+	if (e.data === YT.PlayerState.ENDED) {
+		e.target.loadVideoById({
+			videoId: youtubeVideoData.id,
+			startSeconds: youtubeVideoData.vars.start,
+			endSeconds: youtubeVideoData.vars.end,
+		})
+	}
+}
 </script>
--- src/utils/getYoutubeVideoUrlData.ts
+++ src/utils/getYoutubeVideoUrlData.ts
@@ -1,8 +1,34 @@
+import type { PlayerVars } from "@vue-youtube/core"
+
 export const getYoutubeVideoUrlData = (url: string) => {
 	const videoId = url.split("embed/")[1].split("?")[0]
 
+	const urlData = new URL(url)
+	const urlParams = new URLSearchParams([
+		...Array.from(urlData.searchParams.entries()),
+	])
+
+	// params
+	const newParams = [
+		{ key: "autoplay", value: "1" },
+		{ key: "mute", value: "1" },
+		{ key: "controls", value: "0" },
+		{ key: "loop", value: "1" },
+	]
+
+	newParams.forEach((i) => {
+		urlParams.delete(i.key)
+		urlParams.append(i.key, i.value)
+	})
+
+	/*
+	For some reason, this param is added automatically
+	and we have to remove it manually to make the loop works properly
+	*/
+	urlParams.delete("playlist")
+
 	return {
 		id: videoId,
-		vars: {},
+		vars: Object.fromEntries(urlParams.entries()) as PlayerVars,
 	}
 }
